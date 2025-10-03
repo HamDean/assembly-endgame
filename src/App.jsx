559e354446ce6ab470f38f4b4e1e6cb320fe2a44
Nurudeen Hamzah <hamdeen247@gmail.com>
2025-10-03 21:10:47 +0000

    Build language block element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import LanguageBlock from "./components/LanguageBlock";
 
 const App = () => {
   return (
     <main>
       <Header />
+      <LanguageBlock />
     </main>
-  )
-}
-export default App
+  );
+};
+export default App;
---
+++ src/components/LanguageBlock.jsx
@@ -0,0 +1,6 @@
+const LanguageBlock = () => {
+  return (
+    <div className="lang-block">Javascript</div>
+  )
+}
+export default LanguageBlock
